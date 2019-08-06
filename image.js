4a292f5ef27af090654e676450273d8d3e3effeb
yoreyore <yoreyoretrumpet@gmail.com>
2019-08-07 02:08:21 +0900

    csvファイルから画像名の配列を生成する形に変更

--- image.js
+++ image.js
@@ -1,15 +1,31 @@
-// 読み込み完了時に次の関数を呼び出す
-window.onload = function() {
-    // 画像を配列化し, 乱数で要素を抜き出す
-    var imglist = new Array(
-        "せい.jpg",
-        "そら.jpg",
-        "せいinかまくら.jpg",
-        "そらonかまくら.jpg",
-        "ラブラブ.png",
-        "ラブラブBB.png"
-    );
-    var selectnum = Math.floor(Math.random() * imglist.length);
-    // 抜き出した要素を HTML に渡す
-    document.getElementById("draw").innerHTML = "<img src=" + imglist[selectnum] + ">";
+//CSVファイルを読み込む関数getCSV()の定義
+function getCSV(){
+    var req = new XMLHttpRequest(); //HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
+    req.open("get", "name.csv", true); //アクセスするファイルを指定
+    req.send(null); //HTTPリクエストの発行
+    
+    //レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ	
+    req.onload = function(){
+    select(convertCSVtoArray(req.responseText)); //読み込んだCSVデータを配列化しselect()に渡す
+    }
 }
+
+//読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
+function convertCSVtoArray(str){ //読み込んだCSVデータが文字列として渡される
+    var result = []; //最終的な二次元配列を入れるための配列
+    var tmp = str.split("\n"); //改行を区切り文字として行を要素とした配列を生成
+
+    //各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
+    for(var i=0;i<tmp.length;++i){
+        result[i] = tmp[i].split(',');
+    }
+    return result; //変換して得られた配列を返す 
+}
+
+//配列から画像を選んでHTML要素を書き換える関数selectを定義
+function select(arr) {
+    var selectnum = Math.floor(Math.random() * arr.length); //ランダムに配列から画像を選ぶ
+    document.getElementById("draw").innerHTML = "<img src=image/" + arr[selectnum] + ">"; //idがdrawのHTML要素を選んだ画像に差し替える
+}
+
+getCSV(); //最初に実行される
